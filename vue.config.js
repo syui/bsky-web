c85c080cc95ac805992933929106e725511b4a1f
syui <syui@syui.cf>
2023-03-03 19:50:31 +0900

    add title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+	chainWebpack: config => {
+		config
+			.plugin('html')
+			.tap(args => {
+				args[0].title = "bsky.syui.cf";
+				return args;
+			})
+	},
 	publicPath: "./",
 	configureWebpack: {
 		output: {
